c65dad22dee4e2cef6f567fd7451da5809680190
Nathan Bell <nathanbell09@gmail.com>
2021-08-12 23:10:06 +0100

    game card refactor

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -5,18 +5,6 @@ const GameCard = ({ game }) => {
   return (
     <div className='card'>
       <img src={game.thumbnail} alt='' />
-      {/* <div className='card-header'>
-        <h3>{game.title}</h3>
-        <p>{game.release_date}</p>
-      </div>
-      <div className='card-body'>
-        <p>{game.short_description}</p>
-      </div>
-      <div className='card-footer'>
-        <p>{game.genre}</p>
-        <p>{game.platform}</p>
-      </div> */}
-
       <div className='card-body'>
         <div className='card-header'>
           <div className='card-title'>
@@ -36,17 +24,12 @@ const GameCard = ({ game }) => {
 
 GameCard.propTypes = {
   game: PropTypes.shape({
-    id: PropTypes.number,
     title: PropTypes.string,
     thumbnail: PropTypes.string,
     short_description: PropTypes.string,
-    game_url: PropTypes.string,
     genre: PropTypes.string,
     platform: PropTypes.string,
-    publisher: PropTypes.string,
-    developer: PropTypes.string,
     release_date: PropTypes.string,
-    freetogame_profile_url: PropTypes.string,
   }).isRequired,
 }
 

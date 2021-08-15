7e2ceffd0f198e850fe3f58a33650d630eca2ba2
Nathan Bell <nathanbell09@gmail.com>
2021-08-15 12:27:28 +0100

    added loader component

--- src/components/GamesList/index.jsx
+++ src/components/GamesList/index.jsx
@@ -1,19 +1,18 @@
 import './index.scss'
 import { useGames } from '../../hooks/use-games'
 import GameCard from '../GameCard'
+import Loader from '../Loader'
 
 const GameList = () => {
   const { isLoading, data, error } = useGames()
-
-  if (isLoading) {
-    return 'Loading...'
-  }
 
   if (error) {
     return <pre>{JSON.stringify(error, null, 2)}</pre>
   }
 
-  return (
+  return isLoading ? (
+    <Loader />
+  ) : (
     <div className='list-wrapper'>
       {data.map((game) => (
         <GameCard key={game.id} game={game} />
